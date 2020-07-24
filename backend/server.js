ef33e49802e635e9e23cc7cb1dfbcfc22bb4972b
skg1998 <sahilkg1998@gmail.com>
2020-07-24 12:18:24 +0530

    working on bug..

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const bodyParser = require('body-parser')
 const Payment = require('./routes/payment')
 
 const app = express();
 const port = process.env.PORT ||5000;
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 
 //Bodyparser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +19,10 @@ app.use(cors());
 
 //Routes
 app.use('/api', Payment);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+  })
 
 app.listen(port, () => {
 	console.log(`server Listening on ${port}`);
